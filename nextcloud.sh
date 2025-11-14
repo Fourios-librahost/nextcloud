6dc1889ff780a3c20cd08fae543955ca012376a7
Théo P <31469670+Fourios-librahost@users.noreply.github.com>
2025-11-14 10:32:27 +0100

    Update Docker installation to specific version 28.3.3
    Specify Docker version to install instead of latest.

--- nextcloud.sh
+++ nextcloud.sh
@@ -14,8 +14,9 @@ $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 
-# apt install docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+# apt install docker version 28.3.3
+DOCKER_VERSION="5:28.3.3-1~debian.$(. /etc/os-release && echo "$VERSION_CODENAME")~$(dpkg --print-architecture)"
+sudo apt-get install docker-ce=$DOCKER_VERSION docker-ce-cli=$DOCKER_VERSION containerd.io -y
 
 # install docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.32.3/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
